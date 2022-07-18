9291e04d2e279199ccfc85d4c904a6d94fa137ab
allenHsu <chaoyu.hsuu@gmail.com>
2022-07-18 14:57:14 +0800

    1.0.3

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const http = require('http')
 const express = require('express')
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+const restaurantList = require('./restaurant.json').results
 const app = express()
 const port = 3000
 // express 預設 hostname = localhost
@@ -11,10 +10,35 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 // 設定靜態樣式
 app.use(express.static('public'))
+// 1. 使用者可以在首頁看到所有餐廳與它們的簡單資料：
+// 餐廳照片 餐廳名稱 餐廳分類 餐廳評分
 // 以下為各種網址請求位置
+// 動態產生餐廳資料
 app.get('/', (req, res) => {
-  res.render('index', { restaurant: restaurantList.results })
+  res.render('index', { restaurantList })
 })
+//2. 使用者可以再點進去看餐廳的詳細資訊：
+// 類別 地址 電話 描述 圖片
+// 卡片點入時，id動態產生
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+// 使用者可以透過搜尋餐廳名稱來找到特定的餐廳
+app.get('/search', (req, res) => {
+  console.log(req.query.keyword)
+  if (!req.query.keywords) {
+    res.redirect("/")
+  }
+  const keyword = req.query.keyword
+  // const keyword = req.query.keywords.trim().toLowerCase()
+  const filterRestaurants = restaurantList.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurantList: filterRestaurants, keyword: keyword })
+})
+// 使用者可以透過搜尋餐廳類別來找到特定的餐廳
+
 
 // 設定網址位置
 app.listen(port, () => {
